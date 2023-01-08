95030963d3c272cedb1e6a31f8f7ab7662573d83
Tubagusraja11 <tubagus29@mhs.pelitabangsa.ac.id>
2023-01-08 22:55:13 +0700

    penambahan file

---
+++ app/Views/template/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+        <p>&copy; 2023 - Tubagus Noor Muhammad Muslim - 312110617 - TI.21.b1 - Universitas Pelita Bangsa - Cikarang Selatan</p>
+    </footer>
+    </div>
+</body>
+</html>
